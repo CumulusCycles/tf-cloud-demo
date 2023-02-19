a579c39d149b5ba338a7ded91fed572ab1818b6f
CumulusCycles <cumuluscycles@gmail.com>
2023-02-19 06:59:35 -0500

    commit

--- ecs.tf
+++ ecs.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "demo_app_cluster" {
 }
 
 resource "aws_ecs_task_definition" "demo_app_task" {
-  family                   = "demo-app-task" 
+  family                   = "demo-app-task"
   container_definitions    = <<DEFINITION
   [
     {
@@ -25,12 +25,12 @@ resource "aws_ecs_task_definition" "demo_app_task" {
   network_mode             = "awsvpc"
   memory                   = 512
   cpu                      = 256
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -45,24 +45,31 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_service" "demo_app_service" {
   name            = "demo-app-service"
-  cluster         = "${aws_ecs_cluster.demo_app_cluster.id}"
-  task_definition = "${aws_ecs_task_definition.demo_app_task.arn}"
+  cluster         = aws_ecs_cluster.demo_app_cluster.id
+  task_definition = aws_ecs_task_definition.demo_app_task.arn
   launch_type     = "FARGATE"
   desired_count   = 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.demo_app_task.family
+    container_port   = 3000
+  }
 
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
     assign_public_ip = true
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
   }
 }
 
-resource "aws_default_vpc" "default_vpc" { }
+resource "aws_default_vpc" "default_vpc" {}
 
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
@@ -77,7 +84,7 @@ resource "aws_default_subnet" "default_subnet_c" {
 }
 
 resource "aws_alb" "application_load_balancer" {
-  name               = "test-lb-tf" 
+  name               = "test-lb-tf"
   load_balancer_type = "application"
   subnets = [
     "${aws_default_subnet.default_subnet_a.id}",
@@ -94,6 +101,44 @@ resource "aws_security_group" "load_balancer_security_group" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
   }
 
   egress {
