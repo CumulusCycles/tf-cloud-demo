c3598073fddaf58042e271a1cfe0348efbf76c3d
CumulusCycles <cumuluscycles@gmail.com>
2023-02-19 06:55:24 -0500

    commit

--- ecs.tf
+++ ecs.tf
@@ -62,10 +62,8 @@ resource "aws_ecs_service" "demo_app_service" {
   }
 }
 
-# Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" { }
 
-# Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
 }
@@ -76,4 +74,32 @@ resource "aws_default_subnet" "default_subnet_b" {
 
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" 
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
