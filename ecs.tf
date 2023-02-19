6ddd9ccc90a45c2a26fdd466849214bb3a562b4e
CumulusCycles <cumuluscycles@gmail.com>
2023-02-19 06:45:43 -0500

    commit

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,50 @@
 resource "aws_ecs_cluster" "demo_app_cluster" {
   name = "demo-app-cluster"
 }
+
+resource "aws_ecs_task_definition" "demo_app_task" {
+  family                   = "demo-app-task" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "demo-app-task",
+      "image": "591864715973.dkr.ecr.us-east-1.amazonaws.com/demo-app-repo:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
